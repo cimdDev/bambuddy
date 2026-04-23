e505bad6383b2eb9df0f9530beed56914fbb7fe0
cimdDev <c.tripod@gmx.ch>
2026-04-24 01:01:40 +0200

    fix(custom): align queue comment save callbacks

--- frontend/src/pages/QueuePage.tsx
+++ frontend/src/pages/QueuePage.tsx
@@ -1500,7 +1500,9 @@ export function QueuePage() {
                     onStop={() => setConfirmAction({ type: 'stop', item })}
                     onRequeue={() => {}}
                     onStart={() => {}}
-                    onUpdateComment={(comment) => updateCommentMutation.mutateAsync({ itemId: item.id, comment })}
+                    onUpdateComment={async (comment) => {
+                      await updateCommentMutation.mutateAsync({ itemId: item.id, comment });
+                    }}
                     timeFormat={timeFormat}
                     hasPermission={hasPermission}
                     canModify={canModify}
@@ -1622,7 +1624,9 @@ export function QueuePage() {
                         onStop={() => {}}
                         onRequeue={() => {}}
                         onStart={() => startMutation.mutate({ id: item.id })}
-                        onUpdateComment={(comment) => updateCommentMutation.mutateAsync({ itemId: item.id, comment })}
+                        onUpdateComment={async (comment) => {
+                          await updateCommentMutation.mutateAsync({ itemId: item.id, comment });
+                        }}
                         timeFormat={timeFormat}
                         isSelected={selectedItems.includes(item.id)}
                         onToggleSelect={() => handleToggleSelect(item.id)}
@@ -1687,7 +1691,9 @@ export function QueuePage() {
                       item={item}
                       onRemove={() => setConfirmAction({ type: 'remove', item })}
                       onRequeue={() => setRequeueItem(item)}
-                      onUpdateComment={(comment) => updateCommentMutation.mutateAsync({ itemId: item.id, comment })}
+                      onUpdateComment={async (comment) => {
+                        await updateCommentMutation.mutateAsync({ itemId: item.id, comment });
+                      }}
                       timeFormat={timeFormat}
                       hasPermission={hasPermission}
                       canModify={canModify}
